7dbacacaa42925df450665ace446746ea3b7ab0d
Roy Geesing <roy.geesing@students.fhnw.ch>
2024-09-17 12:30:25 +0200

    Implement snake.js

--- week01/snake/snake.js
+++ week01/snake/snake.js
@@ -18,11 +18,11 @@ const snake = [
 let food = {x: 15, y: 15};
 
 function snakeEquals(a, b) { 
-	/* fill here */
+	return a?.x === b?.x && a?.y === b?.y;
 }
 
 function changeDirection(orientation) {
-    /* fill here */
+    direction = orientation[orientation.indexOf(direction) + 1];
 }
 
 function start() {
@@ -62,10 +62,10 @@ function nextBoard() {
         food.x = Math.floor(Math.random() * 20);   // place new food at random location
         food.y = Math.floor(Math.random() * 20);
     } else {
-        /* fill here */ // no food found => no growth despite new head => remove last element
+        snake.pop(); // no food found => no growth despite new head => remove last element
     }
 
-    /* fill here */ // put head at front of the list
+    snake.unshift(head); // put head at front of the list
 }
 
 function display(context) {
